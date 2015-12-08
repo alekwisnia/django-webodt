5492bd0514d9fd9b3d606a7cff7e6f6cfb8862e5
artiom <artiomse@gmail.com>
2015-12-08 17:04:29 +0300

    fixes

--- webodt/__init__.py
+++ webodt/__init__.py
@@ -214,7 +214,7 @@ class Document(TextIOBase):
 
     def __init__(self, filename, mode='rb', buffering=1):
         print('filename: {0}, mode: {1}, buffering: {2}'.format(filename, mode, buffering))
-        super().__init__(filename, mode, buffering)
+        super(Document, self).__init__(filename, mode, buffering)
 
     def close(self):
         super().close()
