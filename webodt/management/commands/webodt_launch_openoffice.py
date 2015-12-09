34c2bfdd2f27c8adb20f6b2b6644ead0f0161b7b
artiom <artiomse@gmail.com>
2015-12-09 10:51:38 +0300

    fixes

--- webodt/management/commands/webodt_launch_openoffice.py
+++ webodt/management/commands/webodt_launch_openoffice.py
@@ -22,10 +22,9 @@ class Command(BaseCommand):
     )
 
     office_location_variants = [
-        '/usr/lib/libreoffice/program/soffice.bin',
+        # '/usr/lib/libreoffice/program/soffice.bin',
         '/usr/lib/openoffice/program/soffice.bin',
     ]
-
 
     def handle(self, *args, **options):
         path = self.get_office_path(options['executable'])
